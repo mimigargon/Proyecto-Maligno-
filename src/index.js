c3ab1fc2367ed35d4ea460927e2fed4f7be98f63
glorvega <gloria.vega@bootcamp-upgrade.com>
2022-07-04 23:02:58 +0200

    commented routes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store"; 
+/* import { BrowseRouter as Router } from "react-router-dom"; */
+/* import { Provider } from "react-redux"; */
+/* import store from "./redux/store"; */ 
 import App from "./App";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Provider store={store}>
+    
+      
         <App />
-      </Provider>
-    </Router>
+      
+    
   </React.StrictMode>
 );
