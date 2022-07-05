97f857e3b2b1536a5282a764c728e0fa6cef56a1
mimigargon <mireiag77@gmail.com>
2022-07-05 16:15:17 +0200

    Home, Info, Login, and Main code and style started

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import useTimeout from "../../useTimeOut";
+import "./Info.scss";
 
 function Info() {
-   
   return (
     <div className="info-container">
-      <p>Lorem ipsum....</p>
-      
-        <Link to={"/main"}>
+      <p>
+        Una serie de delitos se han cometido dentro de la Deep Web. El autor es
+        un famoso hacker conocido como 4RCH4N63L.
+      </p>
+      <p>
+        Estos delitos no han podido ser rastreados por el Servicio de
+        Inteligencia debido a la alta encriptación de los datos.
+      </p>
+      <p>Por ello han sido solicitados tus servicios como hacker.</p>
+      <p>
+        Tu misión es conseguir descubrir cuales son los delitos cometidos y
+        desencriptar los datos para obtener las credenciales de 4RCH4N63L. 
+      </p>
+      <p>Debes acabar con su reinado de terror de una vez por todas.</p>
+      <p>No puedes fallar.</p>
+      <p> Buena suerte.</p>
+
       {useTimeout(() => {
-        const button = document.createElement('button'); 
-        button.type = 'button'; 
-        button.innerText = 'CONTINUE TO GAME';
-        button.classList.add('info-btn');
-        document.body.appendChild(button); 
-       
-    }, 5000)}
-        </Link>
-      
-      
-      {console.log("setTimeout() Ejemplo...")}
+        const link = document.createElement("a");
+        link.textContent = "CONTINUE TO GAME";
+        link.classList.add("info-btn");
+        link.href = "/main";
+        document.body.appendChild(link);
+      }, 5000)}
     </div>
   );
 }
