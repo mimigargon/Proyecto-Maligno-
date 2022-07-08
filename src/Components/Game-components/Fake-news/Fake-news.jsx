36d951622ceb2444242d2941bf0c59f6dc092ae0
glorvega <gloria.vega@bootcamp-upgrade.com>
2022-07-08 10:52:11 +0200

    added styles to fakenews

--- src/Components/Game-components/Fake-news/Fake-news.jsx
+++ src/Components/Game-components/Fake-news/Fake-news.jsx
@@ -2,7 +2,82 @@ import React from 'react'
 import "./Fake-news.scss";
 
 const FakeNews = () => {
+  function updateTime(){
 
+//Esta función actualiza en tiempo real la hora local con la plantilla del ordenador
+    var today = new Date();
+    var hours24 = today.getHours();
+    var hours12;
+    var minutes = today.getMinutes();
+    var suffix = '';
+  
+    if (hours24 >= 12) {
+      suffix = " PM";
+      hours12 = hours24 % 12;
+    } else {
+      suffix = " AM";
+      hours12 = hours24;
+    }
+    
+    if (minutes % 10 === 0) {
+      //minutes = minutes + "0";
+    } else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+  
+    var time = hours12 + ":" + minutes + suffix;
+  
+    var timeBox = document.querySelector(".start__time-text");
+  
+    timeBox.innerHTML = time;
+  }
+  
+  setInterval(updateTime, 1000);
+  
+  
+  // Para poder arrastrar los objetos
+  
+  var desktopIcons = document.getElementsByClassName("desktop-icon");
+  
+  for (let i = 0; i < desktopIcons.length; i++) {
+    dragElement(desktopIcons[i]);
+  }
+  
+  function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
   
     return (
     <>
