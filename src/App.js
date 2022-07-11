28979b7c569b0a4a1f6ba74e3e4a336d6b859c74
frariqsan <frariqsan@gmail.com>
2022-07-11 19:01:36 +0200

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./Components/Home/Home";
-// import Main from "./Components/Main/Main";
-// import Info from "./Components/Info/Info";
-// import Login from "./Components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Main from "./Components/Main/Main";
+import Info from "./Components/Info/Info";
+import Login from "./Components/Login/Login";
 import "./App.scss";
-// import Weapons from "./Components/Game-components/Weapons/Weapons";
-// import StolenGoods from "./Components/Game-components/Stolen-goods/Stolen-goods";
+import Weapons from "./Components/Game-components/Weapons/Weapons";
+import StolenGoods from "./Components/Game-components/Stolen-goods/Stolen-goods";
 import Cryptos from "./Components/Game-components/Cryptos/Cryptos";
 
 function App() {
   return (
     <>
-    <Cryptos/>
-      {/* <Routes>
+
+      <Routes>
         <Route path="/game-info" element={<Info />} />
         <Route path="/main" element={<Main />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/stolen" element={<StolenGoods/>}/>
-         <Route path='/weapons' element={<Weapons/>}/> 
+        <Route path='/weapons' element={<Weapons/>}/> 
         <Route path='/cryptos' element={<Cryptos/>}/>
 
-      </Routes> */}
+      </Routes>
     </>
   );
 }
