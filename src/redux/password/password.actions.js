03df02d9737e4d2125773350056a7d5f634e8c26
mimigargon <mireiag77@gmail.com>
2022-07-12 15:43:57 +0200

    Redux in process

--- src/redux/password/password.actions.js
+++ src/redux/password/password.actions.js
@@ -13,3 +13,17 @@ export const storeWeaponPass = (cb) => async (dispatch) => {
     console.log("Error al guardar contraseña", error);
   }
 };
+
+export const storeStolenPass = (cb) => async (dispatch) => {
+    try {
+        const password = await getPassword();
+        dispatch({
+          type: STORE_WEAPON_PASS,
+          payload: password.weapon,
+        });
+        setTimeout(() => cb(), 500); 
+      } catch (error) {
+        console.log("Error al guardar contraseña", error);
+      }
+    
+}
